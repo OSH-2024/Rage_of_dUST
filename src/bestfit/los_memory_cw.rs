accf3ffb5410527a7fa416490f21195114295617
Co336 <cw1@mail.ustc.edu.cn>
2024-05-28 20:33:12 +0800

    update los_memory_cw.rs

--- src/bestfit/los_memory_cw.rs
+++ src/bestfit/los_memory_cw.rs
@@ -1,5 +1,15 @@
 include!("los_multipledlinkhead.rs");
 
+struct LosMemPoolStatus{
+    uw_total_used_size: Cell<u32>,
+    uw_total_free_size: Cell<u32>,
+    uw_max_free_node_size: Cell<u32>,
+    uw_used_node_num: Cell<u32>,
+    uw_free_node_num: Cell<u32>,
+    uw_usage_waterline: Cell<u32>
+}
+
+
 fn Los_Mem_Realloc(pool: *mut std::ffi::c_void, ptr: *mut std::ffi::c_void, size: u32) -> *mut std::ffi::c_void{
     let mut int_save: u32;
     let new_ptr: *mut std::ffi::c_void = std::ptr::null_mut();
@@ -44,4 +54,56 @@ fn Los_Mem_Used_Blks_Get(pool: *mut std::ffi::c_void) -> u32{
 
 
     blknums
+}
+
+fn Los_Mem_Task_Id_Get(ptr: *mut std::ffi::c_void) -> u32{
+
+}
+
+fn Los_Mem_Free_Blks_Get(pool: *mut std::ffi::c_void) -> u32{
+
+}
+
+fn Los_Mem_Last_Used_Get(pool: *mut std::ffi::c_void) -> u32{
+
+}
+
+fn Os_Mem_Reset_End_Node(pool: *mut std::ffi::c_void, pre_addr: u32) ->(){
+    
+}
+
+fn Los_Mem_Pool_Size_Get(pool: *mut std::ffi::c_void) -> u32{
+    if pool == std::ptr::null_mut(){
+        return LOS_NOK;
+    }
+    (*(pool as *mut LosMemPoolInfo)).pool_size
+}
+
+fn Los_Mem_Info_Get(pool: *mut std::ffi::c_void, pool_status: *mut LosMemPoolStatus) -> u32{
+    let pool_info: *mut LosMemPoolInfo = pool as *mut LosMemPoolInfo;
+    let mut ret: u32;
+    let mut int_save: u32;
+    if pool_status == std::ptr::null_mut() {
+        println!("can't use NULL addr to save info\n");
+        return LOS_NOK;
+    }
+    if (pool_info == std::ptr::null_mut()) || (pool as u32 != ((*pool_info).pool) as u32) {
+        println!("wrong mem pool addr: {}, line:{}\n", pool_info, line!());
+        return LOS_NOK;
+    }
+    
+    ret
+}
+
+fn Os_Show_Free_Node(index: u32, length: u32, count_num: *u32) ->(){
+    let mut count: u32 = 0;
+    println!("\n    block size:  ");
+    for count in 0..= length-1 {
+        println!("2^{ }", (index + Os_Min_Multi_Dlnk_Log2!() + count));
+    }
+    println!("\n    node number: ");
+    count = 0;
+    for count in 0..= length-1 {
+        println!("  { }", count_num[count + index]);
+    }
 }
