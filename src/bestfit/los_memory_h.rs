603aa91e887bae14bfac47e450393f67e83faf60
Co336 <cw1@mail.ustc.edu.cn>
2024-05-29 18:30:50 +0800

    add los_memory_h.rs

---
+++ src/bestfit/los_memory_h.rs
@@ -0,0 +1,11 @@
+include!("los_multipledlinkhead.rs");
+
+struct LosMemPoolStatus{
+    uw_total_used_size: Cell<u32>,
+    uw_total_free_size: Cell<u32>,
+    uw_max_free_node_size: Cell<u32>,
+    uw_used_node_num: Cell<u32>,
+    uw_free_node_num: Cell<u32>,
+    uw_usage_waterline: Cell<u32>
+
+}//LOS_MEM_POOL_STATUS
