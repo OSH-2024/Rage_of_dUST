abaa2879ee5a7edcca83d7319da9510ead1c00df
Co336 <cw1@mail.ustc.edu.cn>
2024-06-07 23:25:22 +0800

    update los_memory_h.rs

--- src/bestfit/los_memory_h.rs
+++ src/bestfit/los_memory_h.rs
@@ -9,3 +9,51 @@ struct LosMemPoolStatus{
     uw_usage_waterline: Cell<u32>
 
 }//LOS_MEM_POOL_STATUS
+
+macro_rules! Los_Mem_Check_Level_High {
+    {} => {
+        1
+    };
+}
+
+macro_rules! Los_Mem_Check_Level_Low {
+    {} => {
+        0
+    };
+}
+
+macro_rules! Los_Mem_Check_Level_Disable {
+    {} => {
+        0xff
+    };
+}
+
+macro_rules! Los_Errtype_Error {
+    () => {
+        (0x02 << 24)
+    }
+}
+
+macro_rules! Los_Errno_Os_Id {
+    () => {
+        (0x00 << 16)
+    }
+}
+
+macro_rules! Los_Errno_Os_Error {
+    ($mid: expr, $errno: expr) => {
+        (Los_Errtype_Error!() | Los_Errno_Os_Id!() | (($mid as u32) << 8) | $errno as u32)
+    };
+}
+
+macro_rules! Los_Errno_Memcheck_Wrong_Level {
+    {} => {
+        Los_Errno_Os_Error!(Los_Mod_Mem, 0x4)
+    };
+}
+
+macro_rules! Os_Mem_Align_Size {
+    {} => {
+        std::mem::size_of::<u32>()
+    }
+}
