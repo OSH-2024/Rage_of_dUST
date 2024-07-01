14490426f92518d91cbd357609f1aad18111b983
Co336 <cw1@mail.ustc.edu.cn>
2024-07-01 23:28:32 +0800

    update mem_lock_unlock_h.rs

--- src/bestfit/mem_lock_unlock_h.rs
+++ src/bestfit/mem_lock_unlock_h.rs
@@ -1,11 +1,12 @@
-
+extern crate cortex_m;
 use cortex_m::asm;
+
 
 struct SpinLockS{
     raw_lock: u32   
 }
 
-let mut g_mem_spin: SpinLockS;
+pub static mut g_mem_spin: SpinLockS = SpinLockS{raw_lock: 0};
 
 fn Arch_Int_Lock()->u32{
     let int_save: u32;
@@ -39,24 +40,26 @@ fn Los_Int_Restore(int_save: u32)->(){
     Arch_Int_Restore(int_save);
 }
 
-fn Los_Spin_Lock_Save(lock: *mut SpinLockS, int_save: *mut u32) ->(){
-    lock as std::ffi::c_void;
+fn Los_Spin_Lock_Save(lock: *mut SpinLockS, int_save: *mut u32) ->() {
+    ////lock as std::ffi::c_void;
     *int_save = Los_Int_Lock();
 }
 
-fn Los_Spin_Unlock_Restore(lock: *mut SpinLockS, int_save: u32) ->(){
-    lock as std::ffi::c_void;
+fn Los_Spin_Unlock_Restore(lock: *mut SpinLockS, int_save: u32) ->() {
+    ////lock as std::ffi::c_void;
     Los_Int_Restore(int_save);
 }
 
+#[macro_export]
 macro_rules! Mem_Lock {
-    ($int_save: expr) ->{
-        Los_Spin_Lock_Save(&mut g_mem_spin, &($int_save));
+    ($int_save: expr) =>{
+        Los_Spin_Lock_Save(addr_of_mut!(g_mem_spin), &mut ($int_save));
     };
 }
 
+#[macro_export]
 macro_rules! Mem_Unlock {
-    ($int_save: expr) ->{
-        Los_Spin_Unlock_Restore(&mut g_mem_spin, $int_save);
+    ($int_save: expr) =>{
+        Los_Spin_Unlock_Restore(addr_of_mut!(g_mem_spin), $int_save);
     };
 }
