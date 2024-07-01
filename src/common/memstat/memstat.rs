b6e567059eb4f99765fb30305415570ca2561d70
Yiwen <ywzhang0919@gmail.com>
2024-07-01 21:49:15 +0800

    update los memstat

--- src/common/memstat/memstat.rs
+++ src/common/memstat/memstat.rs
@@ -1,85 +1,92 @@
 use core::str;
 use memstat_h;
 
-LITE_OS_TEXT_MINOR!{} fn OSMemstatTaskUsedInc(mut stat: Memstat, usedSize: u32, taskId: u32){
-    let record :u32 = MIN_TASK_ID!(taskId, TASK_NUM - 1); 
-    let mut taskMemstats = &stat.taskMemstats;
+fn Os_Memstat_Task_Used_Inc(stat: &mut Memstat, used_size: u32, task_id: u32) {
+    let record = MIN_TASK_ID!(task_id, TASK_NUM - 1);
+    let task_memstats = &mut stat.task_memstats;
 
-    taskMemstats[record].memUsed += usedSize;
-    taskMemstats[record].memPeak = MAX_MEM_USE!(taskMemstats[record].memPeak, taskMemstats[record].memUsed);
+    task_memstats[record as usize].mem_used += used_size;
+    task_memstats[record as usize].mem_peak = MAX_MEM_USE!(task_memstats[record as usize].mem_peak, task_memstats[record as usize].mem_used);
 
-    stat.memTotalUsed += usedSize;
-    stat.memTotalPeak = MAX_MEM_USE!(stat.memTotalPeak, stat.memTotalUsed); 
+    stat.mem_total_used += used_size;
+    stat.mem_total_peak = MAX_MEM_USE!(stat.mem_total_peak, stat.mem_total_used);
 }
 
-LITE_OS_TEXT_MINOR!{} fn OsMemstatTaskUsedDec(mut stat: Memstat, usedSize: u32, taskId: u32){
-    let record :u32 = MIN_TASK_ID!(taskId, TASK_NUM - 1); 
-    let mut taskMemstats = &stat.taskMemstats;
+fn Os_Memstat_Task_Used_Dec(stat: &mut Memstat, used_size: u32, task_id: u32) {
+    let record = MIN_TASK_ID!(task_id, TASK_NUM - 1);
+    let task_memstats = &mut stat.task_memstats;
 
-    if taskMemstats[record as usize].memUsed < usedSize{
-        print!("mem used of current task '{}': 0x{}, decrease size: 0x{}\n", todo!());
+    if task_memstats[record as usize].mem_used < used_size {
+        println!("mem used of current task '{}':0x{:x}, decrease size:0x{:x}\n",
+                 Os_Curr_Task_Get().task_name, task_memstats[record as usize].mem_used, used_size);
+        return;
     }
 
-    taskMemstats[record as usize].memUsed = 0;
-    taskMemstats[record as usize].memPeak = 0;
+    task_memstats[record as usize].mem_used -= used_size;
+    stat.mem_total_used -= used_size;
 }
 
-LITE_OS_TEXT_MINOR!{} fn OSMemstatTaskClear(mut stat: &Memstat, taskId: u32){
-    let record :u32 = MIN_TASK_ID!(taskId, TASK_NUM - 1); 
-    let mut taskMemstats = &stat.taskMemstats;
+fn Os_Memstat_Task_Clear(stat: &mut Memstat, task_id: u32) {
+    let record = MIN_TASK_ID!(task_id, TASK_NUM - 1);
+    let task_memstats = &mut stat.task_memstats;
 
-    if taskMemstats[record].memUsed != 0{
-        println!("mem used of task '{}' is 0x{}, not zero when task being deleted\n", todo!());
+    if task_memstats[record as usize].mem_used != 0 {
+        println!("mem used of task '{}' is:0x{:x}, not zero when task being deleted\n",
+                 Os_Curr_Task_Get().task_name, task_memstats[record as usize].mem_used);
     }
 
-    taskMemstats[record].memUsed = 0;
-    taskMemstats[record].memPeak = 0;
+    task_memstats[record as usize].mem_used = 0;
+    task_memstats[record as usize].mem_peak = 0;
 }
 
-LITE_OS_TEXT_MINOR!{} fn OsMemstatTaskUsage(stat: &Memstat, taskId: u32)->u32{
+fn OsMemstatTaskUsage(stat: &Memstat, taskId: u32)->u32{
     let record : u32 = MIN_TASK_ID!(taskId, TASK_NUM-1);
-    let taskMemstats = stat.taskMemstats;
+    let task_Memstats = &mut stat.taskMemstats;
 
-    return taskMemstats[record as usize].memUsed;
+    return task_Memstats[record as usize].memUsed;
 }
 
-fn OsMemTaskUsage(taskId: u32)->u32{
-    let mut pool = todo!();
-    let mut stat : &Memstat;
+fn Os_Mem_Task_Usage(task_id: u32) -> u32 {
+    let mut pool: *mut LosMemPoolInfo = std::ptr::null_mut();
+    let mut stat: *mut Memstat = std::ptr::null_mut();
 
-    if cfg!(LOSCFG_MEM_MUL_POOL){
+    #[cfg(not(feature = "LOSCFG_MEM_MUL_POOL"))]
+    {
         /* If Multi-pool is not enabled, then trace SYSTEM MEM only */
-        let pool = todo!();
-        stat = &pool.stat;
-        return OsMemstatTaskUsage(stat, taskId);
+        pool = OS_SYS_MEM_ADDR as *mut LosMemPoolInfo;
+        stat = &mut (*pool).stat;
+        return Os_Memstat_Task_Usage(stat, task_id);
     }
-    else{
-        let inUse:u32 = 0;
-        pool = todo!();
+
+    #[cfg(feature = "LOSCFG_MEM_MUL_POOL")]
+    {
+        let mut in_use = 0;
+        pool = Os_Mem_Mul_Pool_Head_Get();
         while !pool.is_null() {
-            stat = &(pool.stat);
-            inUse += OsMemstatTaskUsage(stat, taskId);
-            pool = pool.nextpool;
+            stat = &mut (*pool).stat;
+            in_use += Os_Memstat_Task_Usage(stat, task_id);
+            pool = (*pool).next_pool;
         }
-        return inUse;
+        return in_use;
     }
 }
 
-fn OsMemTaskClear(taskId: u32){
-    let mut pool = todo!();
-    let mut stat: &Memstat;
+fn Os_Mem_Task_Clear(taskId: u32){
+    let mut pool: *mut LosMemPoolInfo = std::ptr::null_mut();
+    let mut stat: *mut Memstat = std::ptr::null_mut();
 
-    if cfg!(LOSCFG_MEM_MUL_POOL){
-        pool = todo!();
-        stat = &(pool.stat);
-        OSMemstatTaskClear(stat, taskId);
+    #[cfg(not(feature = "LOSCFG_MEM_MUL_POOL"))]{
+        pool = OS_SYS_MEM_ADDR as *mut LosMemPoolInfo;
+        stat = &mut (*pool).stat;
+        Os_Mem_Stat_Task_Clear(stat, taskId);
     }
-    else{
-        pool = todo!();
+
+    #[cfg(feature = "LOSCFG_MEM_MUL_POOL")]{
+        pool = Os_Mem_Mul_Pool_Head_Get();
         while !pool.is_null() {
-            stat = &(pool.stat);
-            OSMemstatTaskClear(stat, taskId);
-            pool = pool.nextPool;
+            stat = &mut (*pool).stat;
+            Os_Mem_Stat_Task_Clear(stat, taskId);
+            pool = (*pool).next_pool;
         }
     }
 }
--- src/common/memstat/memstat_h.rs
+++ src/common/memstat/memstat_h.rs
@@ -1,6 +1,12 @@
 pub const LOSCFG_BASE_CORE_TSK_LIMIT: u32 = 64;
 pub const TASK_NUM: u32 = LOSCFG_BASE_CORE_TSK_LIMIT + 1;
 
+extern "C" {
+    static __heap_start: u8;
+}
+
+let OS_SYS_MEM_ADDR = unsafe { &__heap_start as *const _ as usize };
+
 //重写C头文件中宏定义
 macro_rules! LITE_OS_TEXT_MINOR{() => {}}
 
