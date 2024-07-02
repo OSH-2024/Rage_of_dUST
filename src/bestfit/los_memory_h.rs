26e47c82c6e45131a7c20caad2687ebf484c6622
Co336 <cw1@mail.ustc.edu.cn>
2024-07-02 16:04:27 +0800

    correct los_memory_h.rs

--- src/bestfit/los_memory_h.rs
+++ src/bestfit/los_memory_h.rs
@@ -10,6 +10,25 @@ struct LosMemPoolStatus{
 
 }//LOS_MEM_POOL_STATUS
 
+pub const OS_INVALID: u32 = -1;
+
+enum LOS_MOUDLE_ID {
+    Los_Mod_SYS = 0x0,          /**< System ID. Its value is 0x0. */
+    Los_Mod_Mem = 0x1,          /**< Dynamic memory module ID. Its value is 0x1. */
+    Los_Mod_Tsk = 0x2,          /**< Task module ID. Its value is 0x2. */
+    Los_Mod_Swtmr = 0x3,        /**< Software timer module ID. Its value is 0x3. */
+    Los_Mod_Tick = 0x4,         /**< Tick module ID. Its value is 0x4. */
+    Los_Mod_Msg = 0x5,          /**< Message module ID. Its value is 0x5. */
+    Los_Mod_Que = 0x6,          /**< Queue module ID. Its value is 0x6. */
+    Los_Mod_Sem = 0x7,          /**< Semaphore module ID. Its value is 0x7. */
+    Los_Mod_Mbox = 0x8,         /**< Static memory module ID. Its value is 0x8. */
+    Los_Mod_Mmu = 0xd,          /**< MMU module ID. Its value is 0xd. */
+    Los_Mod_Log = 0xe,          /**< Log module ID. Its value is 0xe. */
+    Los_Mod_Err = 0xf,          /**< Error handling module ID. Its value is 0xf. */
+    Los_Mod_Exc = 0x10,         /**< Exception handling module ID. Its value is 0x10. */
+    Los_Mod_Butt                /**< It is end flag of this enumeration. */
+}
+
 macro_rules! Los_Mem_Check_Level_High {
     {} => {
         1
@@ -49,7 +68,6 @@ macro_rules! Los_Errno_Os_Error {
 macro_rules! Los_Errno_Memcheck_Wrong_Level {
     {} => {
         Los_Errno_Os_Error!(Los_Mod_Mem, 0x4)
-        //枚举
     };
 }
 
