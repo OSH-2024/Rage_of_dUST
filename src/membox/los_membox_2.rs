aa9a197d133f6768cb5b2c8aecee12beb3f54704
Yang Yibo <yangyibo@mail.ustc.edu.cn>
2024-07-05 12:30:07 +0800

    membox

--- src/membox/los_membox_2.rs
+++ src/membox/los_membox_2.rs
@@ -25,14 +25,13 @@
  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * --------------------------------------------------------------------------- */
-// extern crate libc;
-use libc::c_void;
+// use libc::c_void;
 use std::convert::TryInto;
 
-extern "C" {
-    // 声明 C 库中的 memset 函数
-    fn memset(dest: *mut std::ffi::c_void, c: i32, n: u64) -> *mut std::ffi::c_void;
-}
+// extern "C" {
+//     // 声明 C 库中的 memset 函数
+//     fn memset(dest: *mut std::ffi::c_void, c: i32, n: usize) -> *mut std::ffi::c_void;
+// }
 //头文件los_membox.h
 fn los_membox_aligned(mem_addr: usize) -> usize {
     let size_of_usize = core::mem::size_of::<usize>();
@@ -63,11 +62,11 @@ macro_rules! securec_likely {
     };
 }
 
-macro_rules! securec_unlikely {
-    ($x:expr) => {
-        ($x) //__builtin_expect(!!(x), 0)
-    };
-}
+// macro_rules! securec_unlikely {
+//     ($x:expr) => {
+//         ($x) //__builtin_expect(!!(x), 0)
+//     };
+// }
 
 macro_rules! securec_mem_max_len {
     () => {{
@@ -76,47 +75,47 @@ macro_rules! securec_mem_max_len {
 }
 //securecutil.h
 //参考https://course.rs/advance/unsafe/inline-asm.html
-macro_rules! securec_memory_barrier {
-    use std::arch::asm;
-    ($dest:expr) => {{
-        unsafe {
-            asm!("mfence", "memory", "volatile");
-        }
-    }};
-}
-macro_rules! securec_memset_func_opt {
-    ($dest:expr,$value:expr,$count:expr) => {{
-        //此处调用lib/libc/string/memset.c中的memset函数
-        unsafe {
-            libc::memset($dest as *mut std::ffi::c_void, $value, $count);
-        }
-    }};
-}
-macro_rules! securec_memset_prevent_dse {
-    ($dest:expr,$value:expr,$count:expr) => {{
-        securec_memset_func_opt!($dest, $value, $count);
-        securec_memory_barrier!($dest);
-    }};
-}
+// macro_rules! securec_memory_barrier {
+//     use std::arch::asm;
+//     ($dest:expr) => {{
+//         unsafe {
+//             asm!("mfence", "memory", "volatile");
+//         }
+//     }};
+// }
+// macro_rules! securec_memset_func_opt {
+//     ($dest:expr,$value:expr,$count:expr) => {{
+//         //此处调用lib/libc/string/memset.c中的memset函数
+//         unsafe {
+//             libc::memset($dest as *mut std::ffi::c_void, $value, $count);
+//         }
+//     }};
+// }
+// macro_rules! securec_memset_prevent_dse {
+//     ($dest:expr,$value:expr,$count:expr) => {{
+//         securec_memset_func_opt!($dest, $value, $count);
+//         securec_memory_barrier!($dest);
+//     }};
+// }
 
-//memset_s.c
-macro_rules! securec_memset_param_ok {
-    ($dest:expr, $dest_max:expr, $count:expr) => {{
-        (securec_likely!($dest_max) <= securec_mem_max_len!())
-            && (!($dest).is_null())
-            && (($count) <= ($dest_max))
-    }};
-}
-unsafe fn memset_s(dest: *mut std::ffi::c_void, dest_max: u64, c: i32, count: u64) -> i32 {
-    if securec_memset_param_ok!(dest, dest_max, count) {
-        securec_memset_prevent_dse!(dest, c, count);
-        return 0; //EOK
-    }
-    return 0;
-    //TODO
-    /* Meet some runtime violation, return error code */
-    // return SecMemsetError(dest, dest_max, c);
-}
+// //memset_s.c
+// macro_rules! securec_memset_param_ok {
+//     ($dest:expr, $dest_max:expr, $count:expr) => {{
+//         (securec_likely!($dest_max) <= securec_mem_max_len!())
+//             && (!($dest).is_null())
+//             && (($count) <= ($dest_max))
+//     }};
+// }
+// unsafe fn memset_s(dest: *mut std::ffi::c_void, dest_max: u64, c: i32, count: u64) -> i32 {
+//     if securec_memset_param_ok!(dest, dest_max, count) {
+//         securec_memset_prevent_dse!(dest, c, count as usize);
+//         return 0; //EOK
+//     }
+//     return 0;
+//     //TODO
+//     /* Meet some runtime violation, return error code */
+//     // return SecMemsetError(dest, dest_max, c);
+// }
 
 //对应c中LOSCFG_AARCH64的宏定义
 #[cfg(target_arch = "aarch64")]
@@ -135,8 +134,8 @@ unsafe fn os_membox_check_magic<T>(addr: *mut T) -> u32 {
         1 //LOS_NOK
     }
 }
-unsafe fn os_membox_user_addr<T>(addr: *mut T) -> *mut () {
-    (addr as *mut u8).offset(OS_MEMBOX_NODE_HEAD_SIZE as isize) as *mut ()
+unsafe fn os_membox_user_addr<T>(addr: *mut T) -> *mut std::ffi::c_void {
+    (addr as *mut u8).offset(OS_MEMBOX_NODE_HEAD_SIZE as isize) as *mut std::ffi::c_void
 }
 unsafe fn os_membox_node_addr<T>(addr: *mut T) -> *mut LosMemboxNode {
     (((addr as *mut u8).wrapping_sub(OS_MEMBOX_NODE_HEAD_SIZE)) as *mut std::ffi::c_void)
@@ -144,27 +143,27 @@ unsafe fn os_membox_node_addr<T>(addr: *mut T) -> *mut LosMemboxNode {
 }
 
 //TODO
-use libc::c_uint;
-#[repr(C)]
-#[derive(Debug)]
-struct SPIN_LOCK_S {
-    rawLock: libc::c_uint,
-    //TODO
-}
-extern "C" {
-    fn LOS_SpinLockSave(g_memboxSpin: *mut SPIN_LOCK_S, state: &c_uint);
-    fn LOS_SpinUnlockRestore(g_memboxSpin: *mut SPIN_LOCK_S, state: &c_uint);
-}
-fn membox_lock(state: u32) {
-    unsafe {
-        LOS_SpinLockSave(core::ptr::null_mut(), &state);
-    }
-}
-fn membox_unlock(state: u32) {
-    unsafe {
-        LOS_SpinUnlockRestore(core::ptr::null_mut(), &state);
-    }
-}
+// use libc::c_uint;
+// #[repr(C)]
+// #[derive(Debug)]
+// struct SPIN_LOCK_S {
+//     rawLock: libc::c_uint,
+//     //TODO
+// }
+// extern "C" {
+//     fn LOS_SpinLockSave(g_memboxSpin: *mut SPIN_LOCK_S, state: &c_uint);
+//     fn LOS_SpinUnlockRestore(g_memboxSpin: *mut SPIN_LOCK_S, state: &c_uint);
+// }
+// fn membox_lock(state: u32) {
+//     unsafe {
+//         LOS_SpinLockSave(core::ptr::null_mut(), &state);
+//     }
+// }
+// fn membox_unlock(state: u32) {
+//     unsafe {
+//         LOS_SpinUnlockRestore(core::ptr::null_mut(), &state);
+//     }
+// }
 
 unsafe fn os_check_box_mem(boxInfo: *mut LosMemboxInfo, node: *mut std::ffi::c_void) -> u32 {
     let mut offset;
@@ -189,7 +188,7 @@ unsafe fn los_memboxinit(pool: *mut std::ffi::c_void, poolSize: u32, blkSize: u3
     let mut boxInfo = pool as *mut LosMemboxInfo;
     let mut node: *mut LosMemboxNode;
     let mut index: u32;
-    let mut intSave: u32;
+    // let mut intSave: u32;
 
     if Some(pool as *mut LosMemboxInfo) == None {
         return 1; //LOS_NOK
@@ -204,7 +203,7 @@ unsafe fn los_memboxinit(pool: *mut std::ffi::c_void, poolSize: u32, blkSize: u3
         return 1; //LOS_NOK
     }
 
-    membox_lock(intSave);
+    // membox_lock(intSave);
 
     (*boxInfo).uwBlkSize = los_membox_aligned(blkSize as usize + OS_MEMBOX_NODE_HEAD_SIZE)
         .try_into()
@@ -214,7 +213,7 @@ unsafe fn los_memboxinit(pool: *mut std::ffi::c_void, poolSize: u32, blkSize: u3
     (*boxInfo).uwBlkCnt = 0;
 
     if (*boxInfo).uwBlkNum == 0 || (*boxInfo).uwBlkSize < blkSize {
-        membox_unlock(intSave);
+        // membox_unlock(intSave);
         return 1; //LOS_NOK
     }
 
@@ -234,7 +233,7 @@ unsafe fn los_memboxinit(pool: *mut std::ffi::c_void, poolSize: u32, blkSize: u3
 
     (*node).pstNext = None;
 
-    membox_unlock(intSave);
+    // membox_unlock(intSave);
 
     return 0; //LOS_OK
 }
@@ -242,13 +241,13 @@ unsafe fn los_membox_alloc(pool: *mut std::ffi::c_void) -> *mut std::ffi::c_void
     let mut boxInfo = pool as *mut LosMemboxInfo;
     let mut node: *mut LosMemboxNode;
     let mut nodeTmp: Option<*mut LosMemboxNode> = None;
-    let mut intSave: u32;
+    // let mut intSave: u32;
 
     if Some(pool as *mut LosMemboxInfo) == None {
         return core::ptr::null_mut();
     }
 
-    membox_lock(intSave);
+    // membox_lock(intSave);
     node = &mut (*boxInfo).stFreeList as *mut LosMemboxNode;
     if (*node).pstNext != None {
         nodeTmp = (*node).pstNext;
@@ -258,7 +257,7 @@ unsafe fn los_membox_alloc(pool: *mut std::ffi::c_void) -> *mut std::ffi::c_void
         }
         (*boxInfo).uwBlkCnt += 1;
     }
-    membox_unlock(intSave);
+    // membox_unlock(intSave);
 
     if let Some(node_tmp_inner) = nodeTmp {
         return os_membox_user_addr(node_tmp_inner);
@@ -270,16 +269,16 @@ unsafe fn los_membox_free(pool: *mut std::ffi::c_void, Box: *mut std::ffi::c_voi
     let mut boxInfo = pool as *mut LosMemboxInfo;
     let mut node: *mut LosMemboxNode;
     let mut ret: u32 = 1; //LOS_NOK
-    let mut intSave: u32;
+                          // let mut intSave: u32;
 
     if Some(pool as *mut LosMemboxInfo) == None || Some(Box as *mut LosMemboxInfo) == None {
         return 1; //LOS_NOK
     }
 
-    membox_lock(intSave);
+    // membox_lock(intSave);
     loop {
         node = os_membox_node_addr(Box);
-        if os_check_box_mem(boxInfo, node as *mut ()) != 0 {
+        if os_check_box_mem(boxInfo, node as *mut std::ffi::c_void) != 0 {
             break;
         }
         (*node).pstNext = (*boxInfo).stFreeList.pstNext;
@@ -287,40 +286,47 @@ unsafe fn los_membox_free(pool: *mut std::ffi::c_void, Box: *mut std::ffi::c_voi
         (*boxInfo).uwBlkCnt -= 1;
         break;
     }
-    membox_unlock(intSave);
+    // membox_unlock(intSave);
 
     return ret;
 }
 
 unsafe fn los_membox_clr(pool: *mut std::ffi::c_void, Box: *mut std::ffi::c_void) {
     let mut boxInfo = pool as *mut LosMemboxInfo;
-    let mut intSave: u32;
+    // let mut intSave: u32;
 
     if Some(pool as *mut LosMemboxInfo) == None || Some(Box as *mut LosMemboxInfo) == None {
         return;
     }
 
-    membox_lock(intSave);
-    memset_s(
-        Box,
-        (*boxInfo).uwBlkSize as u64 - OS_MEMBOX_NODE_HEAD_SIZE as u64,
-        0,
-        (*boxInfo).uwBlkSize as u64 - OS_MEMBOX_NODE_HEAD_SIZE as u64,
-    );
-    membox_unlock(intSave);
+    // membox_lock(intSave);
+    unsafe {
+        std::ptr::write_bytes(
+            Box,
+            0,
+            (*boxInfo).uwBlkSize as usize - OS_MEMBOX_NODE_HEAD_SIZE,
+        );
+    }
+    // memset_s(
+    //     Box,
+    //     (*boxInfo).uwBlkSize as u64 - OS_MEMBOX_NODE_HEAD_SIZE as u64,
+    //     0,
+    //     (*boxInfo).uwBlkSize as u64 - OS_MEMBOX_NODE_HEAD_SIZE as u64,
+    // );
+    // membox_unlock(intSave);
 }
 
 unsafe fn los_show_box(pool: *mut std::ffi::c_void) {
     let mut index: u32;
-    let mut intSave: u32;
+    // let mut intSave: u32;
     let mut boxInfo = pool as *mut LosMemboxInfo;
-    let mut node: *mut LosMemboxNode;
+    let mut node: *mut LosMemboxNode = std::ptr::null_mut();
 
     if Some(pool as *mut LosMemboxInfo) == None {
         return;
     }
 
-    membox_lock(intSave);
+    // membox_lock(intSave);
     println!(
         "membox({:p},0x{:x},0x{:x}):\r",
         pool,
@@ -348,11 +354,15 @@ unsafe fn los_show_box(pool: *mut std::ffi::c_void) {
     node = boxInfo.wrapping_add(1) as *mut LosMemboxNode;
     index = 0;
     while index < (*boxInfo).uwBlkNum {
-        println!("({},{:p},{:p})\r", index, node, (*node).pstNext);
+        match (*node).pstNext {
+            Some(p) => println!("({},{:p},{:p})\r", index, node, p),
+            None => println!("({},{:p},None)\r", index, node),
+        };
+
         index += 1;
         node = Os_Membox_Next!(node, (*boxInfo).uwBlkSize);
     }
-    membox_unlock(intSave);
+    // membox_unlock(intSave);
 }
 unsafe fn los_membox_statistics_get(
     boxMem: *const std::ffi::c_void,
